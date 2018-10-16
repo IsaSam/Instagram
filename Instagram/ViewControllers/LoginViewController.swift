48b9a7f3ef140eba1902a624fb5ae988c8f797fa
IsaSam <isaacsamuel27@gmail.com>
2018-10-15 21:03:27 -0400

    Parse setup and authentification

---
+++ Instagram/ViewControllers/LoginViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LoginViewController.swift
+//  Instagram
+//
+//  Created by Isaac Samuel on 10/14/18.
+//  Copyright © 2018 Isaac Samuel. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var usernameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func onSignIn(_ sender: Any) {
+    }
+    
+    @IBAction func onSignUp(_ sender: Any) {
+        let newUser = PFUser()
+        newUser.username = usernameField.text
+        newUser.password = passwordField.text
+        
+        newUser.signUpInBackground{(success: Bool, error: Error?) in
+            if success{
+                print("User created successfully!")
+            }else{
+                //print(error?.localizedDescription?)
+                print(error?.localizedDescription)
+                if error?._code == 202{
+                    print("Account already exists for this username.")
+                    
+                }
+            }
+        }
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
