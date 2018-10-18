9c3ebbdbd3a7d7561879f6b2c912c2c8f5c738f4
IsaSam <isaacsamuel27@gmail.com>
2018-10-17 22:41:05 -0400

    finalized take picture

--- Instagram/ViewControllers/CameraViewController.swift
+++ Instagram/ViewControllers/CameraViewController.swift
@@ -13,7 +13,6 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     override func didReceiveMemoryWarning() {
@@ -21,28 +20,29 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         // Dispose of any resources that can be recreated.
     }
     
-    @IBAction func chooseImage(_ sender: Any) {
+    func InstantiateImagePicker(){
         let vc = UIImagePickerController()
         vc.delegate = self
         let actionSheet = UIAlertController(title: "Photo Source", message: "Choose a source", preferredStyle: .actionSheet)
-            actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(UIAlertAction) in
-
-                if UIImagePickerController.isSourceTypeAvailable(.camera) {
-                    print("Camera is available 📸")
-                        //vc.sourceType = .camera
-                        vc.allowsEditing = true
-                        vc.sourceType = UIImagePickerControllerSourceType.camera
-                } else {
-                    print("Camera 🚫 available so we will use photo library instead")
-                    let alertController = UIAlertController(title: "Camera 🚫", message: "Camera is unavailable on this emulator device - Let's try Photo Library", preferredStyle: .alert)
-                    self.present(alertController, animated: true, completion: nil)
-                    // add the OK action to the alert controller
-                    alertController.addAction(UIAlertAction)
-                    
-                    
-                }
-                self.present(vc, animated: true, completion: nil)
-            }))
+        actionSheet.addAction(UIAlertAction(title: "Camera", style: .default, handler: {(UIAlertAction) in
+            
+            if UIImagePickerController.isSourceTypeAvailable(.camera) {
+                print("Camera is available 📸")
+                //vc.sourceType = .camera
+                vc.allowsEditing = true
+                vc.sourceType = UIImagePickerControllerSourceType.camera
+            } else {
+                print("Camera 🚫 available so we will use photo library instead")
+                let alertController = UIAlertController(title: "Camera 🚫", message: "Camera is unavailable on this emulator device - Let's try Photo Library", preferredStyle: .alert)
+                self.present(alertController, animated: true, completion: nil)
+                // add the OK action to the alert controller
+                alertController.addAction(UIAlertAction)
+                
+                
+            }
+            self.present(vc, animated: true, completion: nil)
+            
+        }))
         actionSheet.addAction(UIAlertAction(title: "Photo Library ...", style: .default, handler: {(UIAlertAction) in
             vc.allowsEditing = true
             vc.sourceType = .photoLibrary
@@ -52,19 +52,9 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         self.present(actionSheet, animated: true, completion: nil)
     }
     
-    /*
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
-        imageView.image = image
-        picker.dismiss(animated: true, completion: nil)
+    @IBAction func chooseImage(_ sender: Any) {
+        InstantiateImagePicker()
     }
-    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-        picker.dismiss(animated: true, completion: nil)
-    }
-    */
-    
-    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         // Get the image captured by the UIImagePickerController
         let originalImage = info[UIImagePickerControllerOriginalImage] as! UIImage
@@ -72,6 +62,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         
         // Do something with the images (based on your use case)
         imageView.image = originalImage
+        //self.tapeCaption.text?.removeAll()
         
         // Dismiss UIImagePickerController to go back to your original view controller
         picker.dismiss(animated: true, completion: nil)
