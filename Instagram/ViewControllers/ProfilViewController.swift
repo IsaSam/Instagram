c2afcf7cb7f92575392590c8f4efc69c59cace43
IsaSam <isaacsamuel27@gmail.com>
2018-10-23 02:58:54 -0400

    created profilViewcontroller

--- Instagram/ViewControllers/ProfilViewController.swift
+++ Instagram/ViewControllers/ProfilViewController.swift
@@ -9,17 +9,25 @@
 import UIKit
 
 class ProfilViewController: UIViewController {
-
+    @IBOutlet weak var profilImageView: UIImageView!
+    @IBOutlet weak var fullNameLabel: UILabel!
+    @IBOutlet weak var statusLabel: UILabel!
+    @IBOutlet weak var galleryLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        /*
         let actionSheet = UIAlertController(title: "Profil", message: "", preferredStyle: .actionSheet)
         actionSheet.addAction(UIAlertAction(title: "View Profile", style: .default, handler: {(UIAlertAction) in}))
         actionSheet.addAction(UIAlertAction(title: "Disconnect Session", style: .default, handler: {(UIAlertAction) in}))
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
-        self.present(actionSheet, animated: true, completion: nil)
+        self.present(actionSheet, animated: true, completion: nil)*/
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func editProfileButton(_ sender: Any) {
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
